3597976abca288c1725a1e675636736f23111ad8
G Vamshi Krishna <vamshikrishnagvk5@gmail.com>
2023-09-26 23:30:53 +0530

    Update generate_audio.py
    using tempfile

--- generate_audio.py
+++ generate_audio.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, send_from_directory, send_file
+from flask import Flask, request, send_file
 from flask_restx import Api, Resource, reqparse
 import pyaudio
-import wave
+import os
+import tempfile
 from pydub import AudioSegment
 
 app = Flask(__name__)
@@ -42,21 +43,28 @@ class VoiceRecorder(Resource):
         stream.close()
         p.terminate()
 
-        wav_filename = "output.wav"
         mp3_filename = "output.mp3"
 
-        wf = wave.open(wav_filename, 'wb')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(sample_format))
-        wf.setframerate(fs)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+        # Create a temporary directory to store the MP3 file
+        tmp_dir = tempfile.mkdtemp()
+        mp3_filename = os.path.join(tmp_dir, mp3_filename)
 
-        audio = AudioSegment.from_wav(wav_filename)
+        audio = AudioSegment(
+            data=b''.join(frames),
+            sample_width=sample_format // 8,
+            frame_rate=fs,
+            channels=channels
+        )
+
+        # Export the audio as an MP3 file
         audio.export(mp3_filename, format="mp3")
 
-        # Return the MP3 file as a downloadable response
-        return send_file(mp3_filename, as_attachment=True)
+        # Check if the MP3 file exists before sending it
+        if os.path.isfile(mp3_filename):
+            # Return the MP3 file as a downloadable response
+            return send_file(mp3_filename, as_attachment=True)
+        else:
+            return {'message': 'MP3 file not found'}, 404
 
 if __name__ == '__main__':
     app.run(debug=True)
